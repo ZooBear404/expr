7e7ed4b1e06711e603a3fbe1f9f5444c133b9789
ZooBear404 <zubairstudytech@gmail.com>
2024-12-22 20:07:55 +0430

    project(main): complete the main file

--- main.c
+++ main.c
@@ -1,7 +1,68 @@
+#include <stdio.h>
+
+#include "log.h"
+#include "stack.h"
 #include "expr.h"
 
 int main(int argc, char **argv) {
 
+  int i, error;
+  long result;
+  struct Stack *stack = initialize_stack(&error);
 
-    return 0;
+  i = 0;
+  while (++i < argc) {
+    int number;
+    char operator;
+
+    if (is_number(argv[i], &number, &error)) {
+      push(stack, number, &error);
+      if (error) {
+        return 1;
+      }
+    } else if (is_operator(argv[i], &operator, &error)) {
+        int number1 = pop(stack, &error);
+        if (error) {
+          return 1;
+        }
+	  
+        int number2 = pop(stack, &error);
+        if (error) {
+          return 1;
+        }
+
+        switch(operator) {
+          case '+':
+            result = number1 + number2;
+            break;
+          case '-':
+            result = number1 - number2;
+            break;
+          case '*':
+            result = number1 * number2;
+            break;
+          case '/':
+            result = number1 / number2;
+            break;
+          default:
+            Log(stdout, ERROR, "operator", "invalid operator");
+        }
+
+        push(stack, result, &error);
+        if (error) {
+          return 1;
+        }
+      }
+      }
+
+    int final_result = pop(stack, &error);
+    if (error) {
+      return 1;
+      }
+
+      printf("result: %d\n", final_result);
+  
+
+
+  return 0;
 }
